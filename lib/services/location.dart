a269b0f7cc52368a35bf0eeb6396ac08a51d069c
NAMSEOK YOO <namseokyoo@NAMSEOKui-MacBookAir.local>
2021-04-13 19:34:08 +0900

    exercise clima flutter

---
+++ lib/services/location.dart
@@ -0,0 +1,20 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  // Location({this.latitude, this.longitude});
+
+  double latitude;
+  double longitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      latitude = position.latitude;
+      longitude = position.longitude;
+      // print(position);
+    } catch (e) {
+      print(e);
+    }
+  }
+}
